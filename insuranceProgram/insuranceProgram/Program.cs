eb029c77e2fc065288bf6ec59729ecd50d7ce2f1
William B McAllister <blakemcallister77@gmail.com>
2018-09-20 00:39:55 -0400

    Insurance Questions page 75

---
+++ insuranceProgram/insuranceProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insuranceProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+            bool duiBool;
+            if (dui.ToLower().Equals("no"))
+            {
+                duiBool = true;
+            }
+            else
+            {
+                duiBool = false;
+            }
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            bool qualified = (age > 15) && (duiBool == true) && (tickets <= 1);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+
+        }
+    }
+}
